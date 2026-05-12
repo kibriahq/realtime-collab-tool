8b108206671d8a691a6fee85c88e2e8ea8cdec12
kibria <mdkibria.dev@gmail.com>
2026-05-12 18:33:06 +0600

    fixed: auth layout code stracture

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,26 +1,9 @@
 "use client";
 
-import { Store } from "@/store";
-import { isTokenExpired } from "@/utils/auth";
-import { useStoreActions, useStoreState, useStoreRehydrated } from "easy-peasy";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import useCheckAuth from "@/hooks/useCheckAuth";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    const rehydrated = useStoreRehydrated();
-    const { isAuth, token } = useStoreState((state: Store) => state.auth);
-    const { logout } = useStoreActions((state: any) => state.auth);
-
-    const router = useRouter();
-
-    useEffect(() => {
-        if (!rehydrated) return;
-
-        if (!isAuth || isTokenExpired(token!)) {
-            logout();
-            router.replace("/login");
-        }
-    }, [rehydrated, isAuth, token]);
+    const { rehydrated, isAuth } = useCheckAuth();
 
     if (!rehydrated) {
         return null;
