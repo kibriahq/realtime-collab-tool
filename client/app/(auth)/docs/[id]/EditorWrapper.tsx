926e818cf13c6b335dde296649a96085abc0606f
kibria <mdkibria.dev@gmail.com>
2026-05-12 20:55:12 +0600

    improved: editorwraper

--- client/app/(auth)/docs/[id]/EditorWrapper.tsx
+++ client/app/(auth)/docs/[id]/EditorWrapper.tsx
@@ -3,7 +3,7 @@
 import { getDoc } from "@/api/doc";
 import { Doc } from "@/types/doc";
 import dynamic from "next/dynamic";
-import { notFound, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import Loader from "@/components/ui/Loader";
@@ -15,29 +15,32 @@ const Editor = dynamic(() => import('@/components/editor/Editor'), {
 
 export default function EditorWrapper({ roomName }: { roomName: string }) {
     const [doc, setDoc] = useState<Doc | null>(null);
+    const [loading, setLoading] = useState(true);
     const router = useRouter();
 
-    const fetchDoc = async () => {
-        try {
-            const doc = await getDoc(roomName);
-            setDoc(doc);
-        } catch (error: any) {
-            toast.error(error?.response.data.msg);
-
-            if (error?.response.status === 403) {
-                router.push('/')
-            } else if (error?.response.status === 404) {
-                router.replace(`/404?msg=${error?.response.data.msg}`)
-            } else {
-                throw new Error(error instanceof Error ? error.message : "Failed to fetch document")
-            }
-        }
-    }
-
     useEffect(() => {
+        const fetchDoc = async () => {
+            try {
+                const data = await getDoc(roomName);
+                setDoc(data);
+            } catch (error: any) {
+                toast.error(error?.response?.data?.msg);
+
+                if (error?.response?.status === 403) {
+                    router.push('/');
+                } else if (error?.response?.status === 404) {
+                    router.replace(`/404?msg=${error?.response?.data?.msg}`);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+
         fetchDoc();
     }, [roomName]);
 
+    if (loading) return <Loader />;
+    if (!doc) return null;
 
-    return <Editor roomName={roomName} doc={doc!} />
+    return <Editor roomName={roomName} doc={doc} />;
 }
