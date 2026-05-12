8100440c4ff70dd7cb80b133d9fe1b55617a93ad
kibria <mdkibria.dev@gmail.com>
2026-05-12 12:30:45 +0600

    fixed: auto logout issue on rapid refresh

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -2,26 +2,35 @@
 
 import { Store } from "@/store";
 import { isTokenExpired } from "@/utils/auth";
-import { useStoreActions, useStoreState } from "easy-peasy";
+import {
+    useStoreActions,
+    useStoreState,
+    useStoreRehydrated,
+} from "easy-peasy";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+    const rehydrated = useStoreRehydrated();
     const { isAuth, token } = useStoreState((state: Store) => state.auth);
     const { logout } = useStoreActions((state: any) => state.auth);
+
     const router = useRouter();
-    const [checked, setChecked] = useState(false);
 
     useEffect(() => {
+        if (!rehydrated) return;
+
         if (!isAuth || isTokenExpired(token!)) {
             logout();
             router.replace("/login");
-        } else {
-            setChecked(true);
         }
-    }, [isAuth, token, router]);
+    }, [rehydrated, isAuth, token]);
 
-    if (!checked) {
+    if (!rehydrated) {
+        return null;
+    }
+
+    if (!isAuth) {
         return null;
     }
 
--- client/store/auth-model.ts
+++ client/store/auth-model.ts
@@ -6,6 +6,7 @@ export type AuthModel = {
   user: User;
   isAuth: any;
   token: string | null;
+
   login: Action<AuthModel, {user: User, token: string}>;
   setUser: Action<AuthModel, User>;
   logout: Action<AuthModel, User>;
@@ -15,14 +16,18 @@ const authModel = persist<AuthModel>(
   {
     user: {},
     token: null,
+
     isAuth: computed((state: any) => !!state.token),
+
     login: action((state, user) => {
       state.user = user;
       state.token = user.token;
     }),
+
     setUser: action((state, user) => {
       state.user = {...state.user, ...user};
     }),
+
     logout: action((state) => {
       state.user = {};
       state.token = null;
