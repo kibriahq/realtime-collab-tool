ad1d4766f7ed4b35149211eded8f63d23f5e0025
kibria <mdkibria.dev@gmail.com>
2026-05-12 18:34:56 +0600

    update: auth layout return null logic

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -5,11 +5,7 @@ import useCheckAuth from "@/hooks/useCheckAuth";
 const Layout = ({ children }: { children: React.ReactNode }) => {
     const { rehydrated, isAuth } = useCheckAuth();
 
-    if (!rehydrated) {
-        return null;
-    }
-
-    if (!isAuth) {
+    if (!rehydrated || !isAuth) {
         return null;
     }
 
