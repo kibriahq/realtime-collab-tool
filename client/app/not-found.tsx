da9e39f36d6480f099a182cd61bcc088cb8e5c13
kibria <mdkibria.dev@gmail.com>
2026-05-12 19:00:11 +0600

    added: error wrapper and updated global not-found page

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,38 +1,28 @@
 'use client'
 
+import ErrorIcon from '@/components/ui/errors/ErrorIcon'
+import ErrorMsg from '@/components/ui/errors/ErrorMsg'
+import ErrorTitle from '@/components/ui/errors/ErrorTitle'
+import ErrorWrapper from '@/components/ui/errors/ErrorWrapper'
+import GoHomeBtn from '@/components/ui/errors/GoHomeBtn'
 import { GlobeX } from 'lucide-react'
-import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 
 const NotFound = () => {
     const searchParams = useSearchParams();
     const msg = searchParams.get('msg');
-  return (
-    <section>
-            <div className="relative min-h-screen w-full">
-                <div className="grid min-h-screen px-8"><div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
-                    <button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg text-sm bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none bg-gray-900" type="button">
-                        <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                            <GlobeX className="h-6 w-6" />
-                        </span>
-                    </button>
-                    <h1 className="block antialiased tracking-normal font-sans text-4xl font-semibold leading-tight text-blue-gray-900 mt-5 !leading-snug lg:text-5xl">
-                        Oops...Error 404
-                    </h1>
-                    <p className="block antialiased font-sans text-xl font-normal leading-relaxed text-gray-700 mt-4 mb-6 w-full md:max-w-full lg:mb-8 lg:max-w-3xl">
-                        {msg || "The page you’re looking for is no longer available!"}
-                    </p>
-                    <div className="flex gap-2">
-                        <Link href="/" className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none w-full px-4 md:w-[9rem]" type="button">
-                            Go To Home
-                        </Link>
-
-                    </div>
-                </div>
-                </div>
+    return (
+        <ErrorWrapper>
+            <ErrorIcon>
+                <GlobeX className="h-6 w-6" />
+            </ErrorIcon>
+            <ErrorTitle title="Oops...Error 404" />
+            <ErrorMsg msg={msg || "The page you’re looking for is no longer available!"} />
+            <div className="flex gap-2">
+                <GoHomeBtn />
             </div>
-        </section>
-  )
+        </ErrorWrapper>
+    )
 }
 
 export default NotFound
