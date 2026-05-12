d8b1b94aaf2bb7cc5758390475e4dc402a00ef3a
kibria <mdkibria.dev@gmail.com>
2026-05-12 21:21:59 +0600

    updated: editor header

--- client/components/editor/Header.tsx
+++ client/components/editor/Header.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { Check, SquarePen, Trash, X } from "lucide-react";
 import AddUser from "./AddUser";
 import Avatars from "./ui/Avatars";
 import useEditorHeader from "@/hooks/useEditorHeader";
 import { DocAuthor, Permission } from "@/types/doc";
+import Title from "./ui/Title";
 
 
 function Header({ title, docId, permissions, isAuthor, author }: { title: string, docId: string, permissions: Permission[], isAuthor: boolean, author: DocAuthor }) {
@@ -22,37 +22,24 @@ function Header({ title, docId, permissions, isAuthor, author }: { title: string
 
     return (
         <div className="flex flex-col min-[450px]:flex-row justify-between items-start">
-            <div className="flex items-center gap-2 justify-center">
-                <h1 className="text-3xl font-medium mb-2">
-                    {isAuthor ? (
-                        isEdit === false ? (
-                            <span className="flex items-center gap-3">
-                                {input}
-                                <span onClick={() => setIsEdit(!isEdit)} className="inline text-slate-400 hover:text-slate-500 cursor-pointer"><SquarePen size={20} /></span>
-                            </span>
-                        ) : (
-                            <div className="flex items-center gap-2">
-                                <input onChange={(e) => setInput(e.target.value)} type="text" value={input} className="border border-slate-400 rounded w-[200px] sm:w-[300px]" />
-                                <button onClick={handleUpdateTitle} className="p-1 bg-gray-500 hover:bg-green-800 text-white rounded cursor-pointer"><Check /></button>
-                                <button onClick={() => setIsEdit(!isEdit)} className="p-1 bg-gray-500 hover:bg-red-800 text-white rounded cursor-pointer"><X /></button>
-                            </div>
-                        )
-                    ) : (
-                        <span className="flex items-center gap-3">
-                            {input}
-                        </span>
-                    )}
-
-                </h1 >
-                {isAuthor && (
-                    <button onClick={handleDeleteDoc} className="text-red-300 hover:text-red-400 pb-2 cursor-pointer">
-                        <Trash size={20} />
-                    </button>
-                )}
-            </div>
+            <Title
+                isAuthor={isAuthor}
+                isEdit={isEdit}
+                input={input}
+                setInput={setInput}
+                setIsEdit={setIsEdit}
+                handleUpdateTitle={handleUpdateTitle}
+                handleDeleteDoc={handleDeleteDoc}
+            />
 
             <div className="flex items-center gap-2">
-                <Avatars permissions={perms} handleRemovePermission={handleRemovePermission} isAuthor={isAuthor} author={author} />
+                <Avatars
+                    permissions={perms}
+                    handleRemovePermission={handleRemovePermission}
+                    isAuthor={isAuthor}
+                    author={author}
+                />
+
                 {isAuthor && (
                     <AddUser docId={docId} handleUpdatePermissions={handleUpdatePermissions} />
                 )}
