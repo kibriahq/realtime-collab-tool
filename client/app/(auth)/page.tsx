f92db01c57da7248558017205955fbef5faefda7
kibria <mdkibria.dev@gmail.com>
2026-05-12 18:29:07 +0600

    fixed: home page code stracture

--- client/app/(auth)/page.tsx
+++ client/app/(auth)/page.tsx
@@ -1,54 +1,14 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import { FileText, Plus, Users } from "lucide-react"
-import { createDoc, getMyDocs, getSharedDocs } from "@/api/doc"
-import { useRouter } from "next/navigation"
-import Navbar from "@/components/ui/Navbar"
-import { Doc } from "@/lib/types/doc";
-import DocList from "@/components/ui/DocList"
-import { toast } from "sonner"
+import { FileText, Users } from "lucide-react";
+import Navbar from "@/components/ui/Navbar";
+import DocList from "@/components/ui/DocList";
+import useHome from "@/hooks/useHome";
+import CreateDocButton from "@/components/ui/CreateDocButton";
+import HomeHeader from "@/components/ui/HomeHeader";
 
 export default function Home() {
-  const [docs, setDocs] = useState<Doc[]>([])
-  const [shareDocs, setShareDocs] = useState<Doc[]>([])
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const fetchDocs = async () => {
-      try {
-        const docs = await getMyDocs();
-        setDocs(docs);
-      } catch (error: any) {
-        toast.error(error?.response.data.msg)
-      }
-    }
-    fetchDocs();
-  }, [])
-
-  useEffect(() => {
-    const fetchSharedDocs = async () => {
-      try {
-        const docs = await getSharedDocs();
-        setShareDocs(docs);
-      } catch (error: any) {
-        toast.error(error?.response.data.msg)
-      }
-    }
-    fetchSharedDocs();
-  }, [])
-
-  const createNewDoc = async () => {
-    try {
-      const d = await createDoc();
-      setDocs(prev => [...prev, d]);
-      router.push(`/docs/${d.id}`);
-      toast.success("Document created successfully");
-    } catch (error: any) {
-      toast.error(error?.response.data.msg)
-    }
-  }
+  const { docs, shareDocs, createNewDoc } = useHome();
 
   return (
     <main className="min-h-screen bg-linear-to-br from-slate-50 via-white to-slate-100">
@@ -57,19 +17,8 @@ export default function Home() {
       <div className="container mx-auto px-6 py-8">
         <section className="mb-8">
           <div className="flex flex-col sm:flex-row items-center justify-between">
-            <div className="w-full sm:w-auto">
-              <h1 className="text-3xl font-bold text-slate-800">Welcome to Collab Tool!</h1>
-              <p className="text-slate-600 py-2 pb-4 md:pb-0">Create Share and Collaborate on documents with your team</p>
-            </div>
-            <button
-              onClick={createNewDoc}
-              className="group w-full sm:w-fit flex items-center gap-3 bg-white text-indigo-600 px-5 py-3 rounded-2xl border border-indigo-200 hover:shadow-lg hover:shadow-indigo-500/30 transition-all duration-300 transform hover:-translate-y-1 cursor-pointer"
-            >
-              <div className="p-2 bg-indigo-600/20 rounded-xl">
-                <Plus size={24} />
-              </div>
-              <span className="text-lg font-semibold">Create New</span>
-            </button>
+            <HomeHeader />
+            <CreateDocButton onClick={createNewDoc} />
           </div>
         </section>
 
