d60d10a869cd916ab83df109ea1d666011e23071
kibria <mdkibria.dev@gmail.com>
2026-05-14 19:15:15 +0600

    removed: search params from not-found page

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -6,18 +6,15 @@ import ErrorTitle from '@/components/ui/errors/ErrorTitle'
 import ErrorWrapper from '@/components/ui/errors/ErrorWrapper'
 import GoHomeBtn from '@/components/ui/errors/GoHomeBtn'
 import { GlobeX } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
 
 const NotFound = () => {
-    const searchParams = useSearchParams();
-    const msg = searchParams.get('msg');
     return (
         <ErrorWrapper>
             <ErrorIcon>
                 <GlobeX className="h-6 w-6" />
             </ErrorIcon>
             <ErrorTitle title="Oops...Error 404" />
-            <ErrorMsg msg={msg || "The page you’re looking for is no longer available!"} />
+            <ErrorMsg msg="The page you’re looking for is no longer available!" />
             <div className="flex gap-2">
                 <GoHomeBtn />
             </div>
